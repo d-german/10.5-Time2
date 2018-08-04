9eae424f1c3cf6598a18689c43882b67ea3d215a
d-german <facmet107@gmail.com>
2018-08-04 15:49:27 -0500

    Added unitTest project

---
+++ Time2/Time2Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Time2Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Time2Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c003a57c-6e20-415f-b436-a83079fe64a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Time2/Time2Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Time2Test
+{
+    [TestClass]
+    public class Time2Test
+    {
+        [TestMethod]
+        public void DefaultConstructorTest()
+        {
+            var time2 = new Time2();
+            Assert.AreEqual(0, time2.Hour);
+            Assert.AreEqual(0, time2.Minute);
+            Assert.AreEqual(0, time2.Second);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void InvalidTimeTest()
+        {
+            // Assert - Expects exception
+            var time2 = new Time2(hour: 27, minute: 74, second: 99);
+
+        }
+
+        [TestMethod]
+        public void ToUniversalStringTest()
+        {
+            var time2 = new Time2(hour: 12, minute: 25, second: 42);
+            var expected = "12:25:42";
+            var actual = time2.ToUniversalString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
